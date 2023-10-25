b7f247e0fde34bd0d7a84e79629fb50b2603a023
dedretrowave <pvrvd0x-mail@yandex.ru>
2023-10-25 19:58:39 +0300

    Add Front End build

--- Core/Router.js
+++ Core/Router.js
@@ -5,6 +5,7 @@ import checkAuth from "./Middleware/checkAuth.js";
 import multer from "multer";
 import {handleValidationErrors} from "./Middleware/ValidationErrors.js";
 import {CommentPresenter} from "./Posts/Comments/Presenter/CommentPresenter.js";
+import path from 'path';
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
@@ -22,7 +23,7 @@ export const Router = (app) => {
   const posts = new PostPresenter();
   const comments = new CommentPresenter();
 
-  app.post('/upload', upload.single('image'),
+  app.post('/api/upload', upload.single('image'),
     (req, res) => {
       res
         .status(200)
@@ -32,17 +33,24 @@ export const Router = (app) => {
         });
     });
 
-  app.post('/auth/register', registerValidator, handleValidationErrors, user.create.bind(user));
-  app.post('/auth/login', loginValidator, handleValidationErrors, user.login.bind(user));
-  app.get('/auth/me', checkAuth, user.getMe.bind(user));
-  app.patch('/auth/:id', checkAuth, user.update.bind(user));
+  app.post('/api/auth/register', registerValidator, handleValidationErrors, user.create.bind(user));
+  app.post('/api/auth/login', loginValidator, handleValidationErrors, user.login.bind(user));
+  app.get('/api/auth/me', checkAuth, user.getMe.bind(user));
+  app.patch('/api/auth/:id', checkAuth, user.update.bind(user));
 
-  app.post('/posts', checkAuth, postValidator, handleValidationErrors, posts.create.bind(posts));
-  app.get('/posts', posts.getAll.bind(posts));
-  app.get('/posts/tags', posts.getAllTags.bind(posts));
-  app.get('/posts/:id', posts.getOne.bind(posts));
-  app.delete('/posts/:id', checkAuth, posts.delete.bind(posts));
-  app.patch('/posts/:id', checkAuth, posts.update.bind(posts));
+  app.post('/api/posts', checkAuth, postValidator, handleValidationErrors, posts.create.bind(posts));
+  app.get('/api/posts', posts.getAll.bind(posts));
+  app.get('/api/posts/tags', posts.getAllTags.bind(posts));
+  app.get('/api/posts/:id', posts.getOne.bind(posts));
+  app.delete('/api/posts/:id', checkAuth, posts.delete.bind(posts));
+  app.patch('/api/posts/:id', checkAuth, posts.update.bind(posts));
 
-  app.post('/comments', checkAuth, commentValidator, handleValidationErrors, comments.create.bind(comments));
+  app.post('/api/comments', checkAuth, commentValidator, handleValidationErrors, comments.create.bind(comments));
+
+  app.get('*', (req, res) => {
+    res
+      .status(200)
+      .sendFile(path
+        .join(`${path.resolve()}/public/index.html`));
+  });
 }
